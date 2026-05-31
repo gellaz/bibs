f2e0fec550fd084ccc39139b47231e4ae01996e4
Marco Gelli <marcogelli1994@gmail.com>
2026-05-31 13:02:58 +0200

    feat(api): export holidays domain module barrel

---
+++ apps/api/src/lib/holidays/index.ts
@@ -0,0 +1,17 @@
+export {
+	addDaysYMD,
+	dowFromYMD,
+	expandRange,
+	makeYMD,
+	ymdToYear,
+} from "./dates";
+export { computeEaster } from "./easter";
+export { getOpenStatus } from "./open-status";
+export { resolveOccurrences, resolveStoreClosedDates } from "./resolve";
+export type {
+	CustomClosure,
+	HolidayDef,
+	HolidayType,
+	OpeningHoursDay,
+	OpenStatus,
+} from "./types";
